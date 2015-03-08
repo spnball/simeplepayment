cd3e90d0b7733d9d6dcd6f5b2cd33d1b79115a89
ball <spnball@gmail.com>
2015-03-08 17:41:01 +0700

    update configuration comment

--- config/payment.php
+++ config/payment.php
@@ -1,20 +1,35 @@
 <?php
 return array(
+        'db' => array(
+                'driver' => 'Pdo_Mysql',
+//                 'database' => 'payment database',
+//                 'username' => 'mysql user',
+//                 'password' => 'mysql password'
+        ),
+
         'payment' => array(
                 'paypal' => array (
                         // strategy classs (required)
-                        'strategy' => '\Payment\Strategy\Paypal\Payment',
+                        'strategy' => '\Payment\Strategy\PayPal\Payment',
 
                         // api config (optional)
-                        'api' => array(
-                                'mode' => 'sandbox',
-                                //'clientId' => 'type an api client id here',
-                                //'secret' => 'type a secret key here'
-                        )
+//                         'api' => array(
+//                                 'sdk' => array(
+//                                         'mode' => 'sandbox',
+//                                 ),
+//                                 'clientId' => "Paypal's client id",
+//                                 'secret' => "Paypal's secret id"
+//                         )
                 ),
                 'braintree' => array (
                         // strategy classs (required)
                         'strategy' => '\Payment\Strategy\Braintree\Payment',
+
+//                         'api' => array(
+//                 	            'merchantId' => "Braintree's merchant id",
+//                                 'publicKey'  => "Braintree's public key",
+//                                 'privateKey' => "Braintree's private key"
+//                         )
                 )
         )
 
